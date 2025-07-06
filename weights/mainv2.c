79cd16f39acbf0a68e3080df8b1dd40a26fa320e
Kenneth Law <z5363506@ad.unsw.edu.au>
2025-07-06 13:55:21 +1000

    Added performance metrics to C implementation

--- weights/mainv2.c
+++ weights/mainv2.c
@@ -7,6 +7,12 @@
 #include "fc_weights.h"
 #include "fc_biases.h"
 
+// For performance metrics
+#include <intrin.h>
+#include <stdint.h>
+#include <time.h>
+#include <windows.h>
+
 #define IMG_SIZE 28
 #define KERNEL_SIZE 3
 #define NUM_KERNELS 8
@@ -97,14 +103,38 @@ void fc(float input[NUM_KERNELS * OUT_SIZE * OUT_SIZE], float output[FC_OUT]) {
 }
 
 // Predict function
-int predict(float image[IMG_SIZE][IMG_SIZE]) {
+int predict(float image[IMG_SIZE][IMG_SIZE],
+    int is_performanceCheckMode,
+    double *total_cycles_conv2d, 
+    double *total_cycles_flatten, 
+    double *total_cycles_fc
+) {
     float conv_out[NUM_KERNELS][OUT_SIZE][OUT_SIZE];
     float flat[NUM_KERNELS * OUT_SIZE * OUT_SIZE];
     float logits[FC_OUT];
 
+    // Measure CPU cycles taken for each major operation
+    LARGE_INTEGER conv2d_start, conv2d_end;
+    LARGE_INTEGER flatten_start, flatten_end;
+    LARGE_INTEGER fc_start, fc_end;
+    
+    // conv2d_start = clock();
+    QueryPerformanceCounter(&conv2d_start);
     conv2d(image, conv_out);
+    // conv2d_end = clock();
+    QueryPerformanceCounter(&conv2d_end);
+
+    // flatten_start = clock();
+    QueryPerformanceCounter(&flatten_start);
     flatten(conv_out, flat);
+    // flatten_end = clock();
+    QueryPerformanceCounter(&flatten_end);
+
+    // fc_start = clock();
+    QueryPerformanceCounter(&fc_start);
     fc(flat, logits);
+    // fc_end = clock();
+    QueryPerformanceCounter(&fc_end);
 
     // Argmax
     int pred = 0;
@@ -114,6 +144,22 @@ int predict(float image[IMG_SIZE][IMG_SIZE]) {
             max_val = logits[i];
             pred = i;
         }
+    }
+
+    if (is_performanceCheckMode) {
+        double total_ticks = (double)(conv2d_end.QuadPart - conv2d_start.QuadPart);
+        total_ticks += (double)(flatten_end.QuadPart - flatten_start.QuadPart);
+        total_ticks += (double)(fc_end.QuadPart - fc_start.QuadPart);
+        // Add the PERCENTAGE of total ticks used to the accumulations.
+        // NOTE: The reason we're tracking the proportions/ratios instead of tallying total ticks
+        // Is because convolution operation uses SO many cycles that the counter variable will quickly
+        // overflow :(
+        *total_cycles_conv2d += (double)(conv2d_end.QuadPart - conv2d_start.QuadPart) / total_ticks;
+        *total_cycles_flatten += (double)(flatten_end.QuadPart - flatten_start.QuadPart) / total_ticks;
+        *total_cycles_fc += (double)(fc_end.QuadPart - fc_start.QuadPart) / total_ticks;
+        // printf("Conv2d (cycles):\t%.2f\n", (double)(conv2d_end.QuadPart - conv2d_start.QuadPart) / total_ticks);
+        // printf("Flatten (cycles):\t%.2f\n", (double)(flatten_end.QuadPart - flatten_start.QuadPart) / total_ticks);
+        // printf("FC (cycles):\t\t%.2f\n\n", (double)(fc_end.QuadPart - fc_start.QuadPart) / total_ticks);
     }
     return pred;
 }
@@ -128,8 +174,15 @@ int evaluate_from_csv(const char *filename) {
     char line[4096];
     int total = 0;
     int correct = 0;
+    // FOR PERFORMANCE TESTING ONLY SET maxCOUNT to 0 for default behaviour
+    int maxCount = 1<<30; // Set custom number of samples to evaluate. Leave 0 if all.
+    int counter = 1; // Used for above
+    double total_cycles_conv2d = 0;
+    double total_cycles_flatten = 0;
+    double total_cycles_fc = 0;
 
     while (fgets(line, sizeof(line), file)) {
+        
         // Skip empty lines
         if (strlen(line) < 10) continue;
 
@@ -153,13 +206,36 @@ int evaluate_from_csv(const char *filename) {
         }
         // Predict number from this image
         float probs[FC_OUT];
-        int prediction = predict(input);
+        int prediction = predict(
+            input,
+            maxCount, 
+            &total_cycles_conv2d, 
+            &total_cycles_flatten, 
+            &total_cycles_fc
+        );
         if (prediction == label) correct++;
         total++;
+
+        if (maxCount) {
+            if (counter == maxCount) break;
+            else counter++;
+        }
     }
     fclose(file);
     float acc = 100.0f * correct / total;
     printf("Accuracy: %d/%d correct %.2f\n", correct, total, acc);
+
+    // Print average cycles usage for each major operation
+    if (maxCount) {
+        double avg_conv2d = total_cycles_conv2d / total;
+        double avg_flatten = total_cycles_flatten / total;
+        double avg_fc = total_cycles_fc / total;
+        // printf("%f\n%f\n%f\n\n", total_cycles_conv2d, total_cycles_flatten, total_cycles_fc);
+        printf("Average Proportion of CPU time for each major operation:\n");
+        printf("Conv2D:\t\t%.2f%\n", avg_conv2d*100);
+        printf("Flatten:\t%.2f%\n", avg_flatten*100);
+        printf("FC:\t\t%.2f%\n", avg_fc*100);
+    }
 }
 
 int main() {
